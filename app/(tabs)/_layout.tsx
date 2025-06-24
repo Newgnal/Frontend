a7cb589a187ee49724472e729a4b22bcd8264c70
subinpar <bagsubin565@gmail.com>
2025-06-25 00:29:54 +0900

    Feat: 탭 기능 추가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,17 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { HapticTab } from "@/components/HapticTab";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+import CommunityIcon from "@/assets/images/community.svg";
+import HomeIcon from "@/assets/images/home.svg";
+import NewsroomIcon from "@/assets/images/newsroom.svg";
+import SettingIcon from "@/assets/images/setting.svg";
+import SimulateIcon from "@/assets/images/simulate.svg";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -14,30 +19,137 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+            position: "absolute",
           },
           default: {},
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "홈",
+          tabBarIcon: ({ focused }) => (
+            <HomeIcon
+              width={28.103}
+              height={28.103}
+              stroke={focused ? "#414141" : "#D9D9D9"}
+            />
+          ),
+          tabBarLabelStyle: {
+            color: "#414141",
+            textAlign: "center",
+            fontFamily: "Pretendard",
+            fontSize: 10.539,
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: 17.564,
+          },
         }}
       />
+
       <Tabs.Screen
-        name="explore"
+        name="newsroom"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "내 뉴스룸",
+          tabBarIcon: ({ focused }) => (
+            <NewsroomIcon
+              width={28.103}
+              height={28.103}
+              stroke={focused ? "#414141" : "#D9D9D9"}
+            />
+          ),
+          tabBarLabelStyle: {
+            color: "#414141",
+            textAlign: "center",
+            fontFamily: "Pretendard",
+            fontSize: 10.539,
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: 17.564,
+          },
+        }}
+      />
+
+      <Tabs.Screen
+        name="community"
+        options={{
+          title: "커뮤니티",
+          tabBarIcon: ({ focused }) => (
+            <CommunityIcon
+              width={28.103}
+              height={28.103}
+              stroke={focused ? "#414141" : "#D9D9D9"}
+              fill="none"
+              strokeWidth={2}
+            />
+          ),
+          tabBarLabelStyle: {
+            color: "#414141",
+            textAlign: "center",
+            fontFamily: "Pretendard",
+            fontSize: 10.539,
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: 17.564,
+          },
+        }}
+      />
+
+      <Tabs.Screen
+        name="simulate"
+        options={{
+          title: "모의투자",
+          tabBarIcon: ({ focused }) => (
+            <SimulateIcon
+              width={28.103}
+              height={28.103}
+              stroke={focused ? "#414141" : "#D9D9D9"}
+              fill="none"
+              strokeWidth={2}
+            />
+          ),
+          tabBarLabelStyle: {
+            color: "#414141",
+            textAlign: "center",
+            fontFamily: "Pretendard",
+            fontSize: 10.539,
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: 17.564,
+          },
+        }}
+      />
+
+      <Tabs.Screen
+        name="setting"
+        options={{
+          title: "설정",
+          tabBarIcon: ({ focused }) => (
+            <SettingIcon
+              width={28.103}
+              height={28.103}
+              stroke={focused ? "#414141" : "#D9D9D9"}
+              fill="none"
+              strokeWidth={2}
+            />
+          ),
+          tabBarLabelStyle: {
+            color: "#414141",
+            textAlign: "center",
+            fontFamily: "Pretendard",
+            fontSize: 10.539,
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: 17.564,
+          },
         }}
       />
     </Tabs>
---
+++ app/(tabs)/setting.tsx
@@ -0,0 +1,3 @@
+export default function SettingScreen() {
+  return null; // 혹은 <View><Text>설정</Text></View>
+}
