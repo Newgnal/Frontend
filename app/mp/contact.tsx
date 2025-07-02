7d3d9995dc0fd7fe41387a8dcb1c7bf211aa4282
DaheeJang <jecydahee77@gmail.com>
2025-07-03 00:09:58 +0900

    Feat: mypage - 1:1 문의 UI 구현

--- app/mp/contact.tsx
+++ app/mp/contact.tsx
@@ -15,6 +15,8 @@ export default function QuestionScreen() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
+  const isValid = email.trim() && title.trim() && content.trim();
+
   return (
     <SafeAreaView style={styles.container}>
       <Header
@@ -29,28 +31,92 @@ export default function QuestionScreen() {
 
       <View style={styles.contents}>
         <View>
-          <Text style={typography.title_t2_18_semi_bold}>제목</Text>
-          <View style={styles.wrapper}>
-            {email.length === 0 && (
-              <Text
-                style={[typography.label_l1_14_regular, styles.placeholder]}
-              >
-                제목을 입력해 주세요. (20자 이내)
-              </Text>
-            )}
-            <TextInput
-              value={title}
-              onChangeText={setTitle}
-              maxLength={20}
-              style={styles.input}
-            />
-            <Text
-              style={[styles.counterText, typography.caption_c1_11_regular]}
-            >
-              <Text style={styles.currentLength}>{title.length}</Text>
-              <Text style={styles.totalLength}> / 20</Text>
+          <View>
+            <Text style={typography.subtitle_s3_15_semi_bold}>
+              답변 받을 이메일 주소
             </Text>
+            <View style={styles.wrapper}>
+              {email.length === 0 && (
+                <Text
+                  style={[typography.label_l1_14_regular, styles.placeholder]}
+                >
+                  이메일 주소
+                </Text>
+              )}
+              <TextInput
+                value={email}
+                onChangeText={setEmail}
+                style={styles.input}
+              />
+            </View>
           </View>
+          <View style={{ marginTop: 20 }}>
+            <Text style={typography.subtitle_s3_15_semi_bold}>제목</Text>
+            <View style={styles.wrapper}>
+              {title.length === 0 && (
+                <Text
+                  style={[typography.label_l1_14_regular, styles.placeholder]}
+                >
+                  제목을 입력해 주세요. (20자 이내)
+                </Text>
+              )}
+              <TextInput
+                value={title}
+                onChangeText={setTitle}
+                maxLength={20}
+                style={styles.input}
+              />
+              <Text
+                style={[styles.counterText, typography.caption_c1_11_regular]}
+              >
+                <Text style={styles.currentLength}>{title.length}</Text>
+                <Text style={styles.totalLength}> / 20</Text>
+              </Text>
+            </View>
+          </View>
+          <View style={{ marginTop: 20 }}>
+            <Text style={typography.subtitle_s3_15_semi_bold}>문의 내용</Text>
+            <View style={[styles.wrapper, { minHeight: 220 }]}>
+              {content.length === 0 && (
+                <Text
+                  style={[typography.label_l1_14_regular, styles.placeholder]}
+                >
+                  문의 내용을 입력해 주세요.
+                </Text>
+              )}
+              <TextInput
+                value={content}
+                onChangeText={setContent}
+                maxLength={3000}
+                style={styles.input}
+              />
+              <Text
+                style={[styles.counterText, typography.caption_c1_11_regular]}
+              >
+                <Text style={styles.currentLength}>{content.length}</Text>
+                <Text style={styles.totalLength}> / 3000</Text>
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View
+          style={{
+            backgroundColor: isValid ? "#2E3439" : "#F4F5F7",
+            borderRadius: 12,
+          }}
+        >
+          <Pressable style={[{ alignItems: "center", paddingVertical: 12.5 }]}>
+            <Text
+              style={[
+                typography.subtitle_s3_15_semi_bold,
+                {
+                  color: isValid ? "#F4F5F7" : "#717D89",
+                },
+              ]}
+            >
+              등록하기
+            </Text>
+          </Pressable>
         </View>
       </View>
     </SafeAreaView>
