237b8430f31eccf5accd95a92878dee7d9dc519a
DaheeJang <jecydahee77@gmail.com>
2025-07-05 23:19:21 +0900

    Feat: 커뮤니티 - 인기테마 UI

--- app/(tabs)/community.tsx
+++ app/(tabs)/community.tsx
@@ -1,6 +1,9 @@
 import PlusIcon from "@/assets/images/ic_add.svg";
 import SearchIcon from "@/assets/images/ic_search.svg";
+import FireIcon from "@/assets/images/mingcute_fire-fill.svg";
 import { Header } from "@/components/ui/Header";
+import { HorizontalLine } from "@/components/ui/HorizontalLine";
+import { typography } from "@/styles/typography";
 // import { useRouter } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -23,23 +26,45 @@ export default function MyPageScreen() {
 
         <View style={styles.contents}>
           <View>
-            <Text>인기테마</Text>
-            <View>
-              <View>
+            <View style={{ flexDirection: "row", paddingBottom: 8 }}>
+              <FireIcon />
+              <Text style={typography.subtitle_s2_16_semi_bold}>인기 테마</Text>
+            </View>
+            <View style={styles.rankbox}>
+              <View style={styles.rankdetail}>
                 <Text>1위</Text>
-                <Text>반도체</Text>
+                <Text
+                  style={[styles.rankname, typography.caption_c2_12_regular]}
+                >
+                  반도체/AI
+                </Text>
               </View>
-              <View>
+              <View style={styles.rankdetail}>
                 <Text>2위</Text>
-                <Text>IT/인터넷</Text>
+                <Text
+                  style={[styles.rankname, typography.caption_c2_12_regular]}
+                >
+                  IT/인터넷
+                </Text>
               </View>
-              <View>
+              <View style={styles.rankdetail}>
                 <Text>3위</Text>
-                <Text>모빌리티</Text>
+                <Text
+                  style={[styles.rankname, typography.caption_c2_12_regular]}
+                >
+                  모빌리티
+                </Text>
               </View>
             </View>
-            <View>
-              <Text>지금 뜨는 이야기</Text>
+            <View style={{ paddingTop: 20 }}>
+              <Text
+                style={[
+                  typography.subtitle_s2_16_semi_bold,
+                  { paddingBottom: 16 },
+                ]}
+              >
+                지금 뜨는 이야기
+              </Text>
               <View>
                 <Text>1</Text>
               </View>
@@ -50,18 +75,23 @@ export default function MyPageScreen() {
                 <Text>3</Text>
               </View>
             </View>
-            <View>
-              <Text>방금 올라온 글</Text>
-              <View>
-                <Text>게시글 1</Text>
-              </View>
-              <View>
-                <Text>게시글 2</Text>
-              </View>
-              <View>
-                <Text>게시글 3</Text>
-              </View>
-            </View>
+          </View>
+        </View>
+        <HorizontalLine color={"#F4F5F7"} height={8} />
+        <View style={styles.contents}>
+          <Text
+            style={[typography.subtitle_s2_16_semi_bold, { paddingBottom: 16 }]}
+          >
+            방금 올라온 글
+          </Text>
+          <View>
+            <Text>게시글 1</Text>
+          </View>
+          <View>
+            <Text>게시글 2</Text>
+          </View>
+          <View>
+            <Text>게시글 3</Text>
           </View>
         </View>
       </SafeAreaView>
@@ -77,8 +107,26 @@ const styles = StyleSheet.create({
 
   contents: {
     justifyContent: "center",
-    alignItems: "center",
-    marginTop: 32,
-    // alignSelf: "center",
+
+    padding: 20,
+  },
+
+  rankbox: {
+    backgroundColor: "#F7F7F8",
+    borderRadius: 12,
+    flexDirection: "row",
+    gap: 16,
+    padding: 12,
+  },
+  rankdetail: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  rankname: {
+    backgroundColor: "#484F56",
+    borderRadius: 10,
+    paddingHorizontal: 8,
+    color: "#FFF",
+    textAlign: "center",
   },
 });
